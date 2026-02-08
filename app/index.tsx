cebb1aa028a04d488b50366af3a9dbfb740f3463
aryansaini-71 <aryan9817692114@gmail.com>
2026-02-07 23:51:25 -0700

    Adding Alert Logic

--- app/index.tsx
+++ app/index.tsx
@@ -1,25 +1,33 @@
 import React from 'react';
-import { StyleSheet, View, Text, ScrollView } from 'react-native';
+
+import { StyleSheet, View, Text, ScrollView, TouchableOpacity, Alert } from 'react-native';
 
 export default function App() {
+
+  const handleAlertPress = () => {
+    Alert.alert("Alert Button pressed"); 
+  };
+
   return (
     <View style={styles.screen}>
       <ScrollView>
-        <Text style={styles.placeholderText}>Project</Text>
+        <Text style={styles.placeholderText}>Project Skeleton Created</Text>
       </ScrollView>
+
+      <View style={styles.footer}>
+        <TouchableOpacity style={styles.button} onPress={handleAlertPress}>
+          <Text style={styles.buttonText}>Alert</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  screen: {
-    flex: 1,
-    backgroundColor: '#fff',
-    paddingTop: 50, 
-  },
-  placeholderText: {
-    textAlign: 'center',
-    marginTop: 20,
-    color: '#aaa',
-  },
+  screen: { flex: 1, backgroundColor: '#fff', paddingTop: 50 },
+  placeholderText: { textAlign: 'center', marginTop: 20, color: '#aaa' },
+
+  footer: { padding: 20, borderTopWidth: 1, borderColor: '#eee' },
+  button: { backgroundColor: '#000', padding: 15, borderRadius: 10, alignItems: 'center' },
+  buttonText: { color: '#fff', fontWeight: 'bold' },
 });
