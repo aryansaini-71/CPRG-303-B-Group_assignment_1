f98f5fc466f78388ddc316f2eabea4b6d0c4a2cc
aryansaini-71 <aryan9817692114@gmail.com>
2026-02-07 23:55:12 -0700

    Added Layout for Placeholder

--- app/index.tsx
+++ app/index.tsx
@@ -1,33 +1,71 @@
 import React from 'react';
-
-import { StyleSheet, View, Text, ScrollView, TouchableOpacity, Alert } from 'react-native';
-
+import { StyleSheet, View, Text, TouchableOpacity, Alert, ScrollView } from 'react-native';
+ 
 export default function App() {
-
+ 
   const handleAlertPress = () => {
-    Alert.alert("Alert Button pressed"); 
+    Alert.alert("Alert Button pressed");
   };
-
+ 
   return (
+ 
     <View style={styles.screen}>
+ 
       <ScrollView>
-        <Text style={styles.placeholderText}>Project Skeleton Created</Text>
+ 
+        <View style={styles.headerBox}>
+          <Text style={styles.textStyle}>[Member 2]</Text>
+        </View>
+ 
+        <View style={styles.bodyBox}>
+          <Text style={styles.textStyle}>[Member 3]</Text>
+        </View>
+ 
       </ScrollView>
-
+ 
       <View style={styles.footer}>
         <TouchableOpacity style={styles.button} onPress={handleAlertPress}>
           <Text style={styles.buttonText}>Alert</Text>
         </TouchableOpacity>
       </View>
+ 
     </View>
   );
 }
-
+ 
 const styles = StyleSheet.create({
-  screen: { flex: 1, backgroundColor: '#fff', paddingTop: 50 },
-  placeholderText: { textAlign: 'center', marginTop: 20, color: '#aaa' },
-
-  footer: { padding: 20, borderTopWidth: 1, borderColor: '#eee' },
-  button: { backgroundColor: '#000', padding: 15, borderRadius: 10, alignItems: 'center' },
-  buttonText: { color: '#fff', fontWeight: 'bold' },
+  screen: {
+    flex: 1,
+    backgroundColor: '#ffffff',
+    paddingTop: 50,
+  },
+  headerBox: {
+    height: 200,
+    backgroundColor: '#f9f9f9',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  bodyBox: {
+    height: 400,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  textStyle: {
+    color: '#aaa',
+  },
+  footer: {
+    padding: 20,
+    borderTopWidth: 1,
+    borderColor: '#eee',
+  },
+  button: {
+    backgroundColor: '#000',
+    padding: 15,
+    borderRadius: 10,
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: '#fff',
+    fontWeight: 'bold',
+  },
 });
